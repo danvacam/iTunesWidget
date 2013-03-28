680472909f1a73e3866d9dd40cb3738676ca908f
danvacam <brusaurio@gmail.com>
2013-03-28 10:43:26 +0100

    Update jquery.itunes.js

--- jquery.itunes.js
+++ jquery.itunes.js
@@ -1,6 +1,17 @@
 function itunes (searchterm) {
+    var retObj = {
+        resultCount: 0,
+        results: []
+    };
     $.getJSON('http://itunes.apple.com/search?term=' + searchterm,
     function (data) {
-        return data;
+        retObj.resultCount = data.resultCount;
+        $.each(data.results, function(i, row) {
+                        if(row.artistName !== undefined) {
+                            retObj.results.push(row.artistName);
+                        }
+                    });
+                    
     });
+    return retObj;
 }
